e4f0402681d680fa19191195d3a98937480dd883
Lautaro <lautarodm98@gmail.com>
2024-04-05 16:38:55 -0300

    colores traficker

---
+++ src/data/projects.js
@@ -0,0 +1,24 @@
+export const projects = [
+  {
+    title: "Cooperativa Eléctrica Las Acequias",
+    description:
+      "Tecnologías usadas: HTML, CSS, Javascript, Astro, React.",
+    code: "-",
+    preview: "https://cooperativa-las-acequias.netlify.app/",
+  },
+ 
+  {
+    title: "Municipalidad Las Acequias",
+    description:
+     "Tecnologías usadas: HTML, CSS, Javascript, Astro, React.",
+    code: "-",
+    preview: "https://municipio-las-acequias.netlify.app/",
+  },
+   {
+    title: "Master Servicios Tecnológicos",
+    description:
+      "Tecnologías usadas: en desarrollo.",
+    code: "-",
+    preview: "https://master-servicios-tecnologicos.netlify.app/",
+  }
+];
---
+++ src/data/social.js
@@ -0,0 +1,22 @@
+export const social = {
+  email: {
+    name: "lautarodm98@gmail.com",
+    link: "mailto:lautarodm98@gmail.com"
+  },
+  phone: {
+    name: "+54 9 358 510 6415",
+    link: "tel:+54 9 358 510 6415"
+  },
+  address: {
+    name: "San Luis Potosí, SL",
+    link: "https://goo.gl/maps/1GwLkq7s9kx6qRnTA"
+  },
+  github: {
+    name: "Lautaro Della Mea",
+    link: "https://github.com/lautarodellamea"
+  },
+  linkedin: {
+    name: "Lautaro Della Mea",
+    link: "https://www.linkedin.com/in/lautaro-della-mea"
+  }
+};
