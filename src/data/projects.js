3d836f3368e25ba5ae496c6b4edcdcecb365c52d
Lautaro <lautarodm98@gmail.com>
2024-04-25 21:12:13 -0300

    hello tabs añadida a projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,22 +3,29 @@ export const projects = [
     title: "Cooperativa Eléctrica Las Acequias",
     description:
       "Tecnologías usadas: HTML, CSS, Javascript, Astro, React.",
-    code: "-",
+    code: "",
     preview: "https://cooperativa-las-acequias.netlify.app/",
   },
- 
+
   {
     title: "Municipalidad Las Acequias",
     description:
-     "Tecnologías usadas: HTML, CSS, Javascript, Astro, React.",
-    code: "-",
+      "Tecnologías usadas: HTML, CSS, Javascript, Astro, React.",
+    code: "",
     preview: "https://municipio-las-acequias.netlify.app/",
   },
-   {
+  {
     title: "Master Servicios Tecnológicos",
     description:
-      "Tecnologías usadas: en desarrollo.",
-    code: "-",
+      "Sitio actualmente en desarrollo.",
+    code: "",
     preview: "https://master-servicios-tecnologicos.netlify.app/",
-  }
+  },
+  {
+    title: "Hello Tabs - Extención para navegadores",
+    description:
+      "Hello Tabs es una extensión para tu navegador que te ayuda a organizar y optimizar tus sesiones de navegación. Consulta el archivo README del repositorio para obtener instrucciones detalladas sobre cómo instalar y utilizar esta extensión",
+    code: "https://github.com/lautarodellamea/tabs-extension-google-chrome",
+    preview: "",
+  },
 ];
